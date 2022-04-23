f3768c37bed4972436040cca0e610ecb95b315fb
PhoenixDev22 <96928181+PhoenixDev22@users.noreply.github.com>
2022-04-23 17:20:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ const imageArray = Array.from(document.querySelectorAll(".img__btn img"));
 const display = document.querySelector(".display");
 const closeImg = document.querySelector(".close-img");
 
+const form = document.querySelector(".btn-add");
+
 let emptyMsgExist =  false;
 let currentIndex ;
 
@@ -76,18 +78,16 @@ function getTheCurrentIndex(){
 
 // toggle the shopping cart items
 cartBtn.addEventListener("click", () => {
-    shoppingCart.classList.toggle("hide-cart");
+    shoppingCart.classList.toggle("hide-cart"); 
 });
 
+
 if(input.value == 0){
-    elCart.classList.add("disappear");
-    cartButton.classList.add("dispaly-none");
+    updateTheCartContentTtoEmpty();
     emptyShoppingCart();    
 }else{
-    elCart.classList.remove("disappear");
-    cartButton.classList.remove("dispaly-none");
+    updateTheCartContent();
 }
-
 checkOutBtn.addEventListener("click", () => {
     // the popup messsage as the shopping successed
     setTimeout(createPopup, 500);
@@ -98,10 +98,8 @@ checkOutBtn.addEventListener("click", () => {
         document.querySelector(".num-of-items").remove();
     }
 
-    elCart.classList.add("disappear");
-    cartButton.classList.add("dispaly-none");
+    updateTheCartContentTtoEmpty();
     emptyShoppingCart();  
-
     shoppingCart.classList.remove("hide-cart");
     
 })
@@ -118,7 +116,6 @@ function createPopup(){
     circlediv.setAttribute("class", "circle");
     popupdiv.setAttribute("class", "popup");
     document.body.appendChild(popupdiv);
-  
 }
 
 
@@ -131,19 +128,17 @@ addToCartBtn.addEventListener("click", () => {
     </div>
     <button id="delete__btn" class="delete__btn ff" type="button" aria-label="delete the selected the selected item in the shopping cart"><svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg></button>   `;
     
+    if(input.value < 0 ) return
+
     if(input.value == 0){
-        elCart.classList.add("disappear");
-        cartButton.classList.add("dispaly-none");
+        updateTheCartContentTtoEmpty();
         if(!emptyMsgExist){
             emptyShoppingCart();  
         }
     }else{
     
-
+        updateTheCartContent();
         elCart.innerHTML = innerCart;
-        elCart.classList.remove("disappear");
-        cartButton.classList.remove("dispaly-none");
-
         if(document.querySelector(".empty-msg")){
             document.querySelector(".empty-msg").remove();
         }
@@ -159,21 +154,26 @@ addToCartBtn.addEventListener("click", () => {
         numberItemsInTheCart.innerHTML = input.value;
         // totalAmount.innerHTML = `$${parseInt(input.value) * 125}.00`;
         totalAmount.innerHTML = `$${(parseInt(input.value) * thePreviousPrice*discount).toFixed(2)}`;
-
         input.value = "0";
 
         // delete button in the shopping cart 
         const deleteBtn = document.getElementById("delete__btn");
         deleteBtn.addEventListener("click", ( )=> {
-            elCart.classList.add("disappear");
-            cartButton.classList.add("dispaly-none");
+            updateTheCartContentTtoEmpty();
             emptyShoppingCart();    
             input.value = "0";
             document.querySelector(".num-of-items").remove(); 
         })
     }
 })
-
+function updateTheCartContentTtoEmpty(){
+    elCart.classList.add("disappear");
+    cartButton.classList.add("dispaly-none");
+}
+function updateTheCartContent(){
+    elCart.classList.remove("disappear");
+    cartButton.classList.remove("dispaly-none");
+}
 // empty the shopping cart function
 function emptyShoppingCart(){
     // create the empty message in the shopping cart
@@ -216,6 +216,7 @@ mainImage.addEventListener("click", () => {
     display.classList.add("display-1");
     overlay.classList.toggle("appear-overlay");
 })
+// when you press on close icon closes the  bigger sized image with a lightbox
 closeImg.addEventListener("click", () => {
     display.classList.remove("display-1");
     overlay.classList.remove("appear-overlay");
@@ -223,4 +224,11 @@ closeImg.addEventListener("click", () => {
 overlay.addEventListener("click", () => {
     display.classList.remove("display-1");
     overlay.classList.remove("appear-overlay");
+})
+
+
+form.addEventListener("submit", (e) => {
+    if(input.value < 0){
+        e.preventDefault;
+    }
 })
